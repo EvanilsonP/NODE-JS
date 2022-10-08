0051b75ca3d81b3176015f4fe6698cb5dc89a7fa
EvanilsonP <pevanilson99@gmail.com>
2022-10-08 20:03:15 -0300

    importing npm modules and using validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,8 @@
+const validator = require('validator');
 const getNotes = require('./notes');
-console.log(getNotes());
+
+const msg = getNotes();
+console.log(msg);
+
+console.log(validator.isEmail('pevanilson99@gmail.com'));
+console.log(validator.isURL("https://www.google.com/"));
