ce10136d92872c6f2d1493abf61b7da990e26d0e
EvanilsonP <pevanilson99@gmail.com>
2022-10-08 19:41:26 -0300

    Importing my own files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,2 @@
-const fs = require('fs');
-
-// fs.writeFileSync("notes.txt", "My name is Evanilson P.");
-
-fs.appendFileSync("notes.txt", " I am 23 and been studying Node js");
+const getNotes = require('./notes');
+console.log(getNotes());
