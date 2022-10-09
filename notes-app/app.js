e441c2fbc6170e3cfe7be81b779cd83e08ef531b
EvanilsonP <pevanilson99@gmail.com>
2022-10-09 20:50:49 -0300

    Adding a note. (Created the following functions: LoadNotes and SaveNotes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
 // const chalk = require ('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 // Customize yargs version
 yargs.version('17.6.0');
@@ -23,8 +23,7 @@ yargs.command({
             type: 'string'
         },
     handler: function(argv) {
-        console.log(`Title: ${argv.title}`); 
-        console.log(`Body: ${argv.body}`);
+        notes.addNotes(argv.title, argv.body);
     }
 });
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,7 +1,46 @@
-
-
-const getNotes = function() {
+const fs = require('fs');
+const getNotes = function () {
     console.log("Your notes...");
 }
 
-module.exports = getNotes;
+const addNotes = function (title, body) {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter((note) => { // Checking weather there is duplica notes or not
+        return note.title === title;
+    });
+
+    if(duplicateNotes.length === 0) { //  This means there is no duplica notes
+        notes.push({
+            title: title,
+            body: body
+        });
+    
+        saveNotes(notes);
+        console.log("New note added!");
+
+    } else {
+        console.log('Note title taken!');
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+
+    } catch (e) {
+        return [];
+    }
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNotes
+}
