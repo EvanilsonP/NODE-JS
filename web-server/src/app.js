30fa56a9fa9c5248186832e61134962a7a78def9
EvanilsonP <pevanilson99@gmail.com>
2022-10-12 15:35:00 -0300

    Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,17 +2,25 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send("Help page")
+    res.send({
+        name: 'Evanilson',
+        age: 23
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>');
 });
 
-app.get('/weather', (req, res) => { res.send('weather page') });
+app.get('/weather', (req, res) => { 
+    res.send({
+        forecast: 'It is snowing real bad.',
+        location: 'New York'
+    });
+});
 
 app.listen(3000, () => console.log("Server is up and running on port 3000."));
