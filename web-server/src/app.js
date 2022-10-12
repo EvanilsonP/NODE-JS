48044ca26994150d9a11c2bc618ef46b96c395fb
EvanilsonP <pevanilson99@gmail.com>
2022-10-12 16:03:52 -0300

    Serving up Static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,15 @@
+const path = require('path');
 const express = require('express');
+
+const publicDirectoryPath = path.join(__dirname, '../public');            // Manipulating directory path and accessing a specific folder
+const publicDirectoryPath2 = path.join(__dirname, '..public/about.html');
+const publicDirectoryPath3 = path.join(__dirname, '..public/help.html');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Weather</h1>');
-});
-
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Evanilson',
-        age: 23
-    });
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About page</h1>');
-});
+app.use(express.static(publicDirectoryPath));
+app.use(express.static(publicDirectoryPath2));
+app.use(express.static(publicDirectoryPath3));
 
 app.get('/weather', (req, res) => { 
     res.send({
