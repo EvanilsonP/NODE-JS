ce6e7165526cb8588cbf58cfa839c23fcd9622a3
EvanilsonP <pevanilson99@gmail.com>
2022-10-12 15:23:54 -0300

    Web server - Hello Express!

--- wheather-app/utils/forecast.js
+++ wheather-app/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = 'api.weatherstack.com/current?access_key=3e86df59719916626c79c072269fa2fb&query='+ latitude + ', '+ longitude +'&units=m';
 
-    request({url: url, json: true}, (error, response) => {
+    request({url: url, json: true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to wheather service!', undefined);
 
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find location!', undefined);
         } else {
             callback(undefined, 
-                console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out`)
+                console.log(`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`)
             );
         }
     });
--- wheather-app/utils/geocode.js
+++ wheather-app/utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('request');
 const geoCode = (address, callback) => {
     const url = 'http://api.ipstack.com/134.201.250.155  ' + encodeURIComponent(address) + ' ?access_key=46307f4fd7bd64a7b36a1ae2024bfe95';
 
-    request({url: url, json: true}, (error, response) => {
+    request({url: url, json: true}, (error, {body}) => {
         if(error) {
             callback("Unable to connect to location services!", undefined);
-        } else if (response.body.length === 0){
+        } else if (body.length === 0){
             callback("Unable to find location. Try another search!");
         } else {
            callback(undefined, {
-                lat: response.body.latitude,
-                long: response.body.longitude,
-                location: response.body.location
+                lat: body.latitude,
+                long: body.longitude,
+                location: body.location
 
            })
         }
