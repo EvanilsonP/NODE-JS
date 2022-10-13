654a717c645fbb2ac4e4ce91ebb3092330ef2599
EvanilsonP <pevanilson99@gmail.com>
2022-10-13 12:36:36 -0300

    Dynamic pages with with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,9 +7,31 @@ const publicHelp = path.join(__dirname, '..public/help.html');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(public));
 app.use(express.static(publicAbout));
 app.use(express.static(publicHelp));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Evanilson P'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Evanilson P'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        name: 'Evanilson P'
+    })
+});
 
 app.get('/weather', (req, res) => { 
     res.send({
