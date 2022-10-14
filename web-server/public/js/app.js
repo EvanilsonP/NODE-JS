c64b44ba9a0a1c917631d3c682fbb4fe8f29632f
EvanilsonP <pevanilson99@gmail.com>
2022-10-14 16:46:27 -0300

    Wiring up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,29 @@
 console.log("Client side javascript file is loaded.");
 
-fetch('http://localhost:3000/weather?address.').then((response) => {
+const weather = document.querySelector('form');
+const search = document.querySelector('input');
+const msg1 = document.getElementById('msg-1');
+const msg2 = document.getElementById('msg-2');
+
+msg1.textContent = '';
+msg2.textContent = '';
+
+weather.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+    console.log(location);
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
    response.json().then((data) => {
         if(data.error) {
-            console.log(data.error)
+            msg1.textContent = data.error;
         } else {
+            msg1.textContent = data.location;
+            msg2.textContent = data.forecast;
             console.log(data.location);
             console.log(data.forecast);
         }
    })
 });
-
+})
