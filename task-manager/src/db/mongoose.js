06f61748d55d3430e6745e3b8edae82e173e68c1
EvanilsonP <pevanilson99@gmail.com>
2022-10-28 20:35:15 -0300

    Creating a Mongoose model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -14,13 +14,34 @@ const User = mongoose.model('User', {               // Defining a model
     }
 });
 
-const me = new User({                               // Creating a instance of the model
-    name: 'Evanilson P.',
-    age: 23
+// const me = new User({                               // Creating a instance of the model
+//     name: 'Evanilson P.',
+//     age: 23
+// });
+
+// me.save().then(() => {                              // Saving to the database
+//     console.log(me);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+const Task = mongoose.model('Tasks', {
+    description: {
+        type: String
+    }, 
+
+    completed: {
+        type: Boolean
+    }
 });
 
-me.save().then(() => {                              // Saving to the database
-    console.log(me);
+const task = new Task({
+    description: 'Housecleaning',
+    completed: true
+});
+
+task.save().then(() => {
+    console.log(task);
 }).catch((e) => {
     console.log(e);
-});
+})
