2909a823526dea227623cce5bbce6453cf533d03
EvanilsonP <pevanilson99@gmail.com>
2022-10-31 14:15:56 -0300

    Resource Creation EndPoints Part 2

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 const User = require('./models/user');
+const Task = require('./models/task');
 
 
 app.use(express.json());
@@ -12,7 +13,17 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
     }).catch((e) => {
         res.status(400).send(e);
     });
