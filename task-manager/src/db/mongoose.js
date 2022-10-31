2fbf1aacd4149dced0d112dc3884f30fd7987282
EvanilsonP <pevanilson99@gmail.com>
2022-10-31 12:23:21 -0300

    Data Validation and Sanitization Part 2

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -30,32 +30,51 @@ const User = mongoose.model('User', {               // Defining a model
                 throw new Error('Age must be a positive number.')
             }
         }
+    },
+
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(password) {
+            if(password.toLowerCase().includes('password')) {
+                throw new Error('Passoword cannot contain "password"');
+            }
+        }
     }
 });
 
-const me = new User({                               // Creating a instance of the model
-    name: '                    Evanilson P.',
-    email: 'EVANILSONP@GMAIL.COM'
-});
-
-me.save().then(() => {                              // Saving to the database
-    console.log(me);
-}).catch((e) => {
-    console.log(e);
-});
-
-const Task = mongoose.model('Tasks', {
-    description: { type: String }, 
-    completed: { type: Boolean }
-});
-
-// const task = new Task({
-//     description: 'Housecleaning',
-//     completed: true
+// const me = new User({                               // Creating a instance of the model
+//     name: '                    Evanilson P.  ',
+//     email: '  EVANILSONP@GMAIL.COM  ',
+//     password: '      IWishIWereRich  '
 // });
 
-// task.save().then(() => {
-//     console.log(task);
+// me.save().then(() => {                              // Saving to the database
+//     console.log(me);
 // }).catch((e) => {
 //     console.log(e);
-// })
+// });
+
+const Task = mongoose.model('Tasks', {
+    description: { 
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    completed: { 
+        type: Boolean,
+        default: false,
+    }
+});
+
+const task = new Task({
+    description: '   eat lunch ',
+});
+
+task.save().then(() => {
+    console.log(task);
+}).catch((e) => {
+    console.log(e);
+})
