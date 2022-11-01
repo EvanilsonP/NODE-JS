3a2c2bfbffe0c4a715b34c711ff300c78a3f11ae
EvanilsonP <pevanilson99@gmail.com>
2022-11-01 20:20:32 -0300

    Express Middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,20 +7,24 @@ const port = process.env.PORT || 3000;
 const app = express();
 require('./db/mongoose');
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET') {
+//         res.send('GET requests are disabled.');
+//     } else {
+//         next();
+//     }
+// });
+
+app.use((req, res, next) => {
+    if(req.method === 'GET' || req.method === 'POST' || req.method === 'PATCH' || req.method === 'DELETE' ) {
+        res.status(503).send('Site is currently down. Check back soon.');
+    } else {
+        next();
+    }
+});
+
 app.use(express.json());
 app.use(userRouters);
 app.use(taskRouters);
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
-
-const jwt = require('jsonwebtoken');
-
-const myFunction = async () => {
-    const token = jwt.sign({_id: 'abc123'}, 'thisismylife', {expiresIn: '7 days'});
-    console.log(token);
-
-    const data = jwt.verify(token, 'thisismylife');
-    console.log(data);
-}
-
-myFunction();
