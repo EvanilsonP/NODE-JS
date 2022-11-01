17af8329bcd134a5bff1b1cbc9acd6e0c9ae0979
EvanilsonP <pevanilson99@gmail.com>
2022-11-01 17:01:42 -0300

    Logging in users

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,57 +2,73 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({               
-    name: { 
-        type: String, 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
         required: true,
         trim: true
     },
-
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
         validate(value) {
-            if(!validator.isEmail(value)) {
+            if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid');
             }
         }
     },
-
-    age: { 
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number.')
-            }
-        }
-    },
-
     password: {
         type: String,
         required: true,
-        trim: true,
         minlength: 7,
-        validate(password) {
-            if(password.toLowerCase().includes('password')) {
-                throw new Error('Passoword cannot contain "password"');
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number');
             }
         }
     }
 });
 
-userSchema.pre('save', async function(next) {
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+
+    return user;
+}
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
     const user = this;
-    if(user.isModified('password')) {
+
+    if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8);
     }
+
     next();
 });
 
-// Creating a collection and setting requirements
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,72 +1,74 @@
-const express = require('express');
+const express = require('express')
+const User = require('../models/user')
 const router = new express.Router();
-const User = require('../models/user');
 
-
-// Create user
 router.post('/users', async (req, res) => {
-    const user = new User(req.body);
+    const user = new User(req.body)
 
     try {
-        await user.save();
-        res.status(201).send(user);
+        await user.save()
+        res.status(201).send(user)
     } catch (e) {
-        res.status(400).send(e);
+        res.status(400).send(e)
     }
 });
 
-// Fetch user by ID
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        res.send(user);
+    } catch (e) {
+        res.status(400).send({error: `Incorrect password.`});
+    }
+});
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users)
+    } catch (e) {
+        res.status(500).send()
+    }
+});
+
 router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
+    const _id = req.params.id;
 
     try {
-        const usersId = await User.findById(_id);
+        const user = await User.findById(_id);
 
-        if(!usersId) {
+        if (!user) {
             return res.status(404).send();
         }
-        res.status(200).send(usersId);
 
+        res.send(user);
     } catch (e) {
-        res.status(500).send(e);
+        res.status(500).send();
     }
-    
 });
 
-// Fetch all users
-router.get('/users', async (req, res) => {
-
-   try {
-      const users = await User.find({});
-      res.status(200).send(users);
-   } catch (e) {
-        res.status(500).send(e);
-   }
-});
-
-router.patch('/users/:id', async(req, res) => {
-    // Preventing non-included fields to be updated
+router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
-    if(!isValidOperation) {
-        return res.status(400).send({error: 'Invalid updates!'});
-    }
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    };
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id);
+        const user = await User.findById(req.params.id);
+
         updates.forEach((update) => user[update] = req.body[update]);
         await user.save();
-        // const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
 
-        if(!user) {
+        if (!user) {
             return res.status(404).send();
         }
-        res.send(user)
 
+        res.send(user)
     } catch (e) {
-        res.status(400).send(e); 
+        res.status(400).send(e)
     }
 });
 
@@ -74,13 +76,13 @@ router.delete('/users/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
 
-        if(!user) {
+        if (!user) {
             return res.status(404).send();
         }
-        res.send(user);
 
+        res.send(user);
     } catch (e) {
-        res.status(500).send(e);
+        res.status(500).send();
     }
 });
 
