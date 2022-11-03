d2490588bed4b324c0b50d6a90c6a4a0fa0cd27d
EvanilsonP <pevanilson99@gmail.com>
2022-11-03 16:23:02 -0300

    Filtering data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -20,9 +20,18 @@ router.post('/tasks', auth, async(req, res) => {
 });
 
 // Fetch all tasks
+// GET /tasks?completed=true
 router.get('/tasks', auth, async (req, res) => {
+    const match = {};
+
+    if(req.query.completed) {
+        match.completed = req.query.completed === 'true';
+    }
     try {
-        await req.user.populate('tasks');
+        await req.user.populate({
+            path: 'tasks',
+            match
+        });
         res.status(200).send(req.user.tasks);
 
     } catch (e) {
