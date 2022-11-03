9c41d27b53ed808c53db4d0f84fd7fffa083f7de
EvanilsonP <pevanilson99@gmail.com>
2022-11-03 19:55:28 -0300

    Adding images for user profile

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -102,7 +102,7 @@ router.delete('/users/me', auth, async (req, res) => {
 });
 
 const upload = multer({
-    dest: 'avatars',
+    // dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -117,10 +117,18 @@ const upload = multer({
 
 });
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async(req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
 }, (error, req, res, next) => {
     res.status(400).send({error: error.message});
+});
+
+router.delet('users/me/avatar', auth, async(req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
 });
 
 module.exports = router;
