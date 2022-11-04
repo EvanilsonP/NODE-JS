4649aebc81e988aabad4f48edb5762c32d723df3
EvanilsonP <pevanilson99@gmail.com>
2022-11-04 16:14:02 -0300

    Writing my own tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math');
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math');
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3);
@@ -8,4 +8,14 @@ test('Should calculate total with tip', () => {
 test('Should calculate total with default tip', () => {
     const total = calculateTip(10);
     expect(total).toBe(12.5);
+});
+
+test('Should convert 32 F to 0 C', () => {
+    const temperature = fahrenheitToCelsius(32);
+    expect(temperature).toBe(0);
+});
+
+test('Should convert 32 F to 0 C', () => {
+    const temperature = celsiusToFahrenheit(0);
+    expect(temperature).toBe(32);
 });
