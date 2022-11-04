98decaf8fc0af8e9375bf5733917f608c5b948df
EvanilsonP <pevanilson99@gmail.com>
2022-11-04 17:41:39 -0300

    Socket.io events

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,9 +12,20 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
+let count = 0;
+// We have a socket.io server that does two things: 
+// server(emit) -> client (receive) - countUpdated
+// client(emit) -> server (receive) - increment
 // Printing a message when a client connects
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New Websocket connection!');
+    socket.emit('countUpdated', count);
+
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count); // Emiting an event for a specific connection
+        io.emit('countUpdated', count);       // This one emits an event for EVERY SINGLE connection
+    });
 });
 
 server.listen(port, () => console.log(`Server is up on port ${port}`));
