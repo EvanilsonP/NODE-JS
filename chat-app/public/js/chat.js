99827dffbb67ceead1be4eec20bb82ee5f02c616
EvanilsonP <pevanilson99@gmail.com>
2022-11-04 18:24:37 -0300

    Socket.io events challenge

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,11 +1,21 @@
 const socket = io();
 // Sending data from server to client;
 // name of the event has to match the one created on socket.emit();
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated.', count);
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated.', count);
+// });
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('clicked');
+//     socket.emit('increment');
+// });
+
+socket.on('message', (message) => {
+    console.log(message);
 });
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('clicked');
-    socket.emit('increment');
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = e.target.elements.message.value; // Targeting input
+    socket.emit('sendMessage', message);
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -11,20 +11,25 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
-
-let count = 0;
+// let count = 0;
 // We have a socket.io server that does two things: 
 // server(emit) -> client (receive) - countUpdated
 // client(emit) -> server (receive) - increment
 // Printing a message when a client connects
 io.on('connection', (socket) => {
     console.log('New Websocket connection!');
-    socket.emit('countUpdated', count);
+    // socket.emit('countUpdated', count);
 
-    socket.on('increment', () => {
-        count++
-        // socket.emit('countUpdated', count); // Emiting an event for a specific connection
-        io.emit('countUpdated', count);       // This one emits an event for EVERY SINGLE connection
+    // socket.on('increment', () => {
+    //     count++
+    //     // socket.emit('countUpdated', count); // Emiting an event for a specific connection
+    //     io.emit('countUpdated', count);       // This one emits an event for EVERY SINGLE connection
+    // });
+
+    socket.emit('message', 'Welcome!'); // Message when you visite the URL;
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
     });
 });
 
