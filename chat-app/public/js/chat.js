e66a1edd7bb60b054fe68e2584220645729fb0ea
EvanilsonP <pevanilson99@gmail.com>
2022-11-05 15:30:24 -0300

    Event acknowledgement

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,24 +1,35 @@
-const socket = io();
+const socket = io()
 // Sending data from server to client;
 // name of the event has to match the one created on socket.emit();
 socket.on('message', (message) => {
-    console.log(message);
+    console.log(message)
 });
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault();
+
     const message = e.target.elements.message.value; // Targeting input
-    socket.emit('sendMessage', message);
+
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error)
+        };
+
+        console.log('Message delivered!');
+    });
 });
 
 document.querySelector('#send-location').addEventListener('click', () => {
     if (!navigator.geolocation) {
-        return alert('Geolocation is not supported by your browser.')
-    }
+        return alert('Geolocation is not supported by your browser.');
+    };
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')  ;
         });
     });
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,36 +1,46 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io'); // Setting up support for socket.io
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io') // Setting up support for socket.io
+const Filter = require('bad-words')
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-const port = process.env.PORT || 4000;
-const publicDirectoryPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 4000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath));
-// Printing a message when a client connects
+
 io.on('connection', (socket) => {
-    console.log('New Websocket connection!');
+    // Printing a message when a client connects
+    console.log('New WebSocket connection');
     // Message when you visite the URL; // emit to a particular connection
-    socket.emit('message', 'Welcome!'); 
-    // Emit a message to everybody but that particular connection.
+    socket.emit('message', 'Welcome!');
+     // Emit a message to everybody but that particular connection.
     socket.broadcast.emit('message', 'A new user has joined!');
 
-    socket.on('sendMessage', (message) => {
-        io.emit('message', message);    // Message to every single client
+    socket.on('sendMessage', (message, callback) => {   // Message to every single client
+        const filter = new Filter();                    // Preventing profanity
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!');
+        }
+
+        io.emit('message', message);
+        callback();
     });
 
-    socket.on('sendLocation', (coords) => {
+    // Sharing my location
+    socket.on('sendLocation', (coords, callback) => {
         io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
-    })
-
+        callback()
+    });
     // When a user disconnect
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!');
+        io.emit('message', 'A user has left!')
     });
 });
 
-server.listen(port, () => console.log(`Server is up on port ${port}`));
+server.listen(port, () => { console.log(`Server is up on port ${port}!`) });
