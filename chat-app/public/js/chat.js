f0ce39a62b3040406a1ea554ea2ec00dda8a0f2e
EvanilsonP <pevanilson99@gmail.com>
2022-11-08 14:18:59 -0300

    Sending messages to rooms

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -17,6 +17,7 @@ const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true}) /
 socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
+        username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a')
     });
@@ -26,6 +27,7 @@ socket.on('message', (message) => {
 socket.on('locationMessage', (message) => {                        // Or url
     console.log(message);
     const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
         url: message.url,                                         // Or url
         createdAt: moment(message.createdAt).format('h:mm a')
     });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -28,13 +28,14 @@ io.on('connection', (socket) => {
         socket.join(user.room);      // Socket.emit: that sends an event to a specific client, io.emmit: sends an event to every connected client
         // io.to.emit: what this does is it emits an event to everybody in a specific room.
         // broadcast.to.emit: This is sending an event to everyone except for the specific client
-        socket.emit('message', generateMessages('Welcome!'));                     // Message when you visite the URL; // emit to a particular connection
+        socket.emit('message', generateMessages('Admin', 'Welcome!'));                     // Message when you visite the URL; // emit to a particular connection
         // socket.broadcast.emit('message', generateMessages('A new user has joined!'));  // Emit a message to everybody but that particular connection.
-        socket.broadcast.to(user.room).emit('message', generateMessages(`${user.username} has joined!`));
+        socket.broadcast.to(user.room).emit('message', generateMessages('Admin', `${user.username} has joined!`));
         callback();
     });
 
     socket.on('sendMessage', (message, callback) => {            // Message to every single client
+        const user = getUser(socket.id);
         const filter = new Filter();                             // Preventing profanity
 
         if (filter.isProfane(message)) {
@@ -42,19 +43,20 @@ io.on('connection', (socket) => {
         }
 
         // io.emit('message', generateMessages(message));
-        io.to('Center City').emit('message', generateMessages(message));
+        io.to(user.room).emit('message', generateMessages(user.username, message));
         callback();
     })
 
     socket.on('sendLocation', (coords, callback) => {           // Sharing location
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
         callback();
     });
 
     socket.on('disconnect', () => {                             // When a user disconnect
         const user = removeUser(socket.id);
         if(user) {
-            io.to(user.room).emit('message', generateMessages(`${user.username} has left!`));
+            io.to(user.room).emit('message', generateMessages('Admin', `${user.username} has left!`));
         }
     });
 });
